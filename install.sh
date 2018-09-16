4178d584d2a201324854cabe6585155d767017fa
koyahee <40119630+koyahee@users.noreply.github.com>
2018-09-17 02:56:18 +0900

    add shutdownbuttond.service

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 sudo cp twelite-read.service /etc/systemd/system/
+sudo cp shutdownbuttond.service /etc/systemd/system/
 sudo systemctl start twelite-read.service
 sudo systemctl enable twelite-read.service
+sudo systemctl start shutdownbuttond.service
+sudo systemctl enable shutdownbuttond.service
